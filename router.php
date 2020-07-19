41c0e67c8a75c32a054700cff03b1eb52f7b7369
Adam McGurk <mcgurk@mcgurkconsulting.com>
2020-07-19 12:56:15 -0500

    Built up the router

---
+++ router.php
@@ -0,0 +1,8 @@
+<?php
+declare(strict_types=1);
+use BaseApi\{Router, RouterException};
+
+require_once 'vendor/autoload.php';
+
+$Router = new Router(\parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
+$Router->route();
